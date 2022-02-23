c6511c58c2cdc4766645a442395a0238e684cb45
Carlos Jaramillo <cejaramillof@mercadolibre.com.co>
2022-02-22 22:16:03 -0500

    refactor: add requiredProperties validation recursive, and add tests

--- src/__tests__/register-vehicle.spec.ts
+++ src/__tests__/register-vehicle.spec.ts
@@ -1,41 +1,21 @@
 import { RegisterVehicle } from '../controllers/register-vehicle'
 
-describe('RegisterVehicle', () => {
-  test('is the name doesn\'t exist return 400', () => {
+describe.each([
+  ['name', { model: 'F400', year: 2022 }],
+  ['model', { name: 'Ferrari', year: 2022 }],
+  ['year', { name: 'Ferrari', model: 'F400' }]
+  // ['xd', { name: 'Ferrari', model: 'F400', year: 2022 }]
+])('RegisterVehicle', (prop: string, body) => {
+  test('is the %s doesn\'t exist return 400', () => {
     // system under test
     const sut = new RegisterVehicle()
-    const httpRequest = {
-      body: {
-        // name: 'Ferrari',
-        model: 'F400',
-        year: 2022
-      }
-    }
+    const httpRequest = { body }
 
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toBeInstanceOf(Error)
-    expect(httpResponse.body).toEqual(new Error('error in the: name'))
-    expect(httpResponse.body.message).toBe('error in the: name')
-  })
-
-  test('is the model doesn\'t exist return 400', () => {
-    // system under test
-    const sut = new RegisterVehicle()
-    const httpRequest = {
-      body: {
-        name: 'Ferrari',
-        // model: 'F400',
-        year: 2022
-      }
-    }
-
-    const httpResponse = sut.handle(httpRequest)
-
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toBeInstanceOf(Error)
-    expect(httpResponse.body).toEqual(new Error('error in the: model'))
-    expect(httpResponse.body.message).toBe('error in the: model')
+    expect(httpResponse.body).toEqual(new Error(`error in the: ${prop}`))
+    expect(httpResponse.body.message).toBe(`error in the: ${prop}`)
   })
 })
