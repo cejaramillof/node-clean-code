d64a54994f686fade82d6782a05979928dc391f5
Carlos Jaramillo <cejaramillof@mercadolibre.com.co>
2022-03-01 21:48:27 -0500

    feat: connect mongoose

---
+++ src/infra/db/mongo/connect.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose'
+
+export default (): void => {
+  void mongoose.connect(
+    (process.env.MONGO_URI as string),
+    {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      dbName: 'tddtest'
+    },
+    (res: any): void => {
+      try {
+        console.log('DB Connected at Port \x1b[32m27017\x1b[0m - \x1b[32m%s\x1b[0m', 'online')
+      } catch (error) {
+        res.status(500).json({
+          ok: false,
+          message: 'Error to connect database'
+        })
+      }
+    })
+}
--- src/main/server/server.ts
+++ src/main/server/server.ts
@@ -1,7 +1,9 @@
+import connectDB from '../../infra/db/mongo/connect'
 import app from '../config/app'
 
 const port = process.env.PORT ?? 3000
 
 app.listen(port, () => {
   console.log(`Server is working at Port: \x1b[32m${port}\x1b[0m`)
+  connectDB()
 })
